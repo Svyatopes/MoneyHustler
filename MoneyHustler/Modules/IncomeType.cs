c242483fc3601b11b9e83d5d5586d096ded1e862
Aufniy Tiger <golenkotoxa@gmail.com>
2021-11-13 20:02:25 +0300

    create class

---
+++ MoneyHustler/Modules/IncomeType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyHustler
+{
+    class IncomeType //класс категории дохода
+    {
+        public string Name { get; set; } //название категории
+    }
+}
---
+++ MoneyHustler/Modules/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyHustler
+{
+    class Person //класс члена семьи
+    {
+        public string Name { get; set; } //имя члена
+    }
+}
