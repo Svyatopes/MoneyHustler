ce16d2fa499a91b9a1b9b5bc23c00feb432cec11
BarBoss2228 <nastia.polpoudenko@gmail.com>
2021-11-14 19:13:04 +0300

    Created class Deposit

---
+++ MoneyHustler/Models/Deposit.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyHustler.Models
+{
+    public abstract class Deposit : MoneyVault //наследуется от мани волт
+    {
+        public decimal Percent { get; set; }
+        public int PaymentDay { get; set; } // день месяца, число не может быть отрицательным и больше 31, продумать логику set(чтобы не нарушать дату начисления процента) 
+
+        public Deposit(decimal percent, int paymentDay) //string name) : base(name) нужно сделать конструктор для мани волт,
+                                                        //чтобы в конструкторе депозита использовать параметры, такие как имя и баланс 
+        {
+            Percent = percent;
+            PaymentDay = paymentDay;
+        }
+
+        //public void DecreaseBalance(decimal value) нужно подумать, зачем переопределять этот метод
+        
+    }
+}
