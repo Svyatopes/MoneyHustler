f20c85e3b04164d41793c361463a6fdc64b3057b
Alice Palermo <petrowa.aliska@yandex.ru>
2021-11-14 19:24:15 +0300

    Storage completed

--- MoneyHustler/Models/Storage.cs
+++ MoneyHustler/Models/Storage.cs
@@ -6,7 +6,52 @@ using System.Threading.Tasks;
 
 namespace MoneyHustler.Models
 {
-    class Class1
+    static class Storage
     {
+
+        //Класс используется как хранилище всех данных в программе.
+
+        public static List<Person> Persons;
+        public static List<MoneyVault> Vaults;
+        public static List<IncomeType> IncomeTypes;
+        public static List<ExpenseType> ExpenseTypes;
+
+        public static List<Income> GetAllIncomes()
+        {
+            List<Income> allIncomes = new List<Income>();
+
+            foreach (MoneyVault moneyVault in Vaults)
+            {
+                if (!allIncomes.Any())
+                {
+                    allIncomes = moneyVault.Incomes;
+                }
+                else
+                {
+                    allIncomes.AddRange(moneyVault.Incomes);
+                }
+            }
+
+            return allIncomes;
+        } 
+
+        public static List<Expense> GetAllExpences()
+        {
+            List<Expense> allExpences = new List<Expense>();
+
+            foreach (MoneyVault moneyVault in Vaults)
+            {
+                if (!allExpences.Any())
+                {
+                    allExpences = moneyVault.Expenses;
+                }
+                else
+                {
+                    allExpences.AddRange(moneyVault.Expenses);
+                }
+            }
+
+            return allExpences;
+        }
     }
 }
