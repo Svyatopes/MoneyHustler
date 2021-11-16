3524970055d99254c4f0d94eed74e0f56bcacc42
svyatopes <svyatopes@gmail.com>
2021-11-16 21:17:12 +0300

    some initial window graphic for incomes

--- MoneyHustler/AuxiliaryWindows/WindowIncomes.xaml.cs
+++ MoneyHustler/AuxiliaryWindows/WindowIncomes.xaml.cs
@@ -1,16 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using MoneyHustler.Models;
 
 namespace MoneyHustler
 {
@@ -19,9 +12,27 @@ namespace MoneyHustler
     /// </summary>
     public partial class WindowIncomes : Window
     {
+        private List<Income> _incomes;
+        private ObservableCollection<Income> sd;
         public WindowIncomes()
         {
             InitializeComponent();
+            _incomes = new List<Income>()
+            {
+                new Income(500,DateTime.Now,new Person(){Name="Бабуля"}, string.Empty,new Card("TestCard",50,0),new IncomeType () {Name="На пирожное"}),
+                new Income(600,DateTime.Now,new Person(){Name="Дедуля"}, "Some",new Card("TestCard",50,0),new IncomeType () {Name="На пирожное"}),
+                new Income(700,DateTime.Now,new Person(){Name="Я"}, "Chtoto",new Card("TestCard",50,0),new IncomeType () {Name="На пирожное"}),
+            };
+            sd = new ObservableCollection<Income>(_incomes);
+            listViewForIncomes.ItemsSource = sd;
+        }
+
+        private void ButtonRemoveIncomeItemClick(object sender, RoutedEventArgs e)
+        {
+            var button = (Button)sender;
+            var income = (Income)button.DataContext;
+            sd.Remove(income);
+            int stop = 1;
         }
     }
 }
