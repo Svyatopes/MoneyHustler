00fd16782390936d8560d70ec33d0670fb2ce6bf
Arctic Beaver <ArcticBeaver@yandex.ru>
2021-11-19 00:11:56 +0300

    Deletion and adding methods are ready

--- MoneyHustler/AuxiliaryWindows/WindowCategories.xaml.cs
+++ MoneyHustler/AuxiliaryWindows/WindowCategories.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Collections.ObjectModel;
 
 namespace MoneyHustler.AuxiliaryWindows
 {
@@ -20,41 +21,50 @@ namespace MoneyHustler.AuxiliaryWindows
     /// </summary>
     public partial class WindowCategories : Window
     {
+        private ObservableCollection<IncomeType> _incomeTypes;
+
+        private ObservableCollection<ExpenseType> _expenseTypes;
+
         public WindowCategories()
         {
             InitializeComponent();
-            FillExpensesList();
-            FillIncomesList();
-        }
 
-        private void FillIncomesList()
-        {
-            ListViewIncomes.Items.Clear();
-            foreach (IncomeType incomeType in Storage.IncomeTypes)
-            {
-                ListViewIncomes.Items.Add(incomeType.Name);
-            }
-        }
+            _incomeTypes = new ObservableCollection<IncomeType>(Storage.IncomeTypes);
+            _expenseTypes = new ObservableCollection<ExpenseType>(Storage.ExpenseTypes);
 
-        private void FillExpensesList()
-        {
-            ListViewExpenses.Items.Clear();
-            foreach (ExpenseType expenseType in Storage.ExpenseTypes)
-            {
-                ListViewExpenses.Items.Add(expenseType.Name);
-            }
+            ListViewIncomes.ItemsSource = _incomeTypes;
+            ListViewExpenses.ItemsSource = _expenseTypes;
         }
 
         private void ButtonRemoveIncomeCategoryClick(object sender, RoutedEventArgs e)
         {
+            var button = (Button)sender;
+            var incomeType = (IncomeType)button.DataContext;
 
-           // if (Storage.IncomeTypes.Contains())
+            if (Storage.GetAllIncomes().Any(item => item.Type == incomeType))
+            {
+                MessageBox.Show("Эта категория используется вами!");
+                return;
+            }    
+
+            Storage.IncomeTypes.Remove(incomeType);
+            _incomeTypes.Remove(incomeType);
+           
         }
 
         private void ButtonRemoveExpenseCategoryClick(object sender, RoutedEventArgs e)
         {
+            var button = (Button)sender;
+            var expenseType = (IncomeType)button.DataContext;
 
-            //if (Storage.IncomeTypes.Contains())
+            if (Storage.GetAllIncomes().Any(item => item.Type == expenseType))
+            {
+                MessageBox.Show("Эта категория используется вами!");
+                return;
+            }
+
+            Storage.IncomeTypes.Remove(expenseType);
+            _incomeTypes.Remove(expenseType);
         }
 
         private void ButtonRenameIncomeCategoryClick(object sender, RoutedEventArgs e)
@@ -65,6 +75,31 @@ namespace MoneyHustler.AuxiliaryWindows
         private void ButtonRenameExpenseCategoryClick(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void ButtonAddIncomeCategory_Click(object sender, RoutedEventArgs e)
+        {
+            if (TextBoxEnterIncomeCategory.Text.Length == 0) return;
+            if (_incomeTypes.Any(item => item.Name == TextBoxEnterIncomeCategory.Text))
+            {
+                MessageBox.Show("Такая категория уже существует!");
+                return;
+            }
+
+            Storage.IncomeTypes.Add(new IncomeType() { Name = TextBoxEnterIncomeCategory.Text });
+            _incomeTypes.Add(new IncomeType() { Name = TextBoxEnterIncomeCategory.Text });
+        }
+
+        private void ButtonAddExpenseCategory_Click(object sender, RoutedEventArgs e)
+        {
+            if (TextBoxEnterExpenseCategory.Text.Length == 0) return;
+            if (_incomeTypes.Any(item => item.Name == TextBoxEnterExpenseCategory.Text))
+            {
+                MessageBox.Show("Такая категория уже существует!");
+                return;
+            }
+            Storage.ExpenseTypes.Add(new ExpenseType() { Name = TextBoxEnterExpenseCategory.Text });
+            _expenseTypes.Add(new ExpenseType() { Name = TextBoxEnterExpenseCategory.Text });
         }
     }
 }
