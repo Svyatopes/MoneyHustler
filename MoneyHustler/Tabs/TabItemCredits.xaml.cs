99035a08767830478af82cb08308a319181a7415
Nikita <klophah@gmail.com>
2021-11-22 17:17:46 +0300

    now edit button work correctly

--- MoneyHustler/Tabs/TabItemCredits.xaml.cs
+++ MoneyHustler/Tabs/TabItemCredits.xaml.cs
@@ -40,22 +40,15 @@ namespace MoneyHustler.Tabs
             ComboBoxCards.ItemsSource = _storageInstance.Vaults;
             ComboBoxPerson.ItemsSource = _storageInstance.Persons;
 
-        }
+            ChangeVisibilityOfGridAddEditVault(false);
 
-        private void Sort(string sortBy, ListSortDirection direction)
-        {
-            ICollectionView dataView = CollectionViewSource.GetDefaultView(listViewForCredits.ItemsSource);
 
-            dataView.SortDescriptions.Clear();
-            SortDescription sd = new SortDescription(sortBy, direction);
-            dataView.SortDescriptions.Add(sd);
-            dataView.Refresh();
         }
 
         private void ButtonAddCreditClick(object sender, RoutedEventArgs e)
         {
-            AuxiliaryWindows.WindowAddEditCredit windowIncomes = new();
-            windowIncomes.ShowDialog();
+
+            ChangeVisibilityOfGridAddEditVault(true);
             UpdateCreditsView();
         }
 
@@ -63,9 +56,39 @@ namespace MoneyHustler.Tabs
         {
             var button = (Button)sender;
             var credit = (Credit)button.DataContext;
+            ChangeVisibilityOfGridAddEditVault(true);
+            _credit = credit;
 
-            AuxiliaryWindows.WindowAddEditCredit windowCredits = new(credit);
-            windowCredits.ShowDialog();
+            if (_credit != null)
+            {
+                TextBoxValueWithoutPercent.IsEnabled = false;
+                DatePickerDayOpen.IsEnabled = false;
+                DatePickerDayClose.IsEnabled = false;
+                TextBoxPercent.IsEnabled = false;
+            }
+            else
+            {
+                TextBoxValueWithoutPercent.IsEnabled = true;
+                DatePickerDayOpen.IsEnabled = true;
+                DatePickerDayClose.IsEnabled = true;
+                TextBoxPercent.IsEnabled = true;
+            }
+
+            TextBoxName.Text = _credit.Name;
+
+            TextBoxValueWithoutPercent.Text = Convert.ToString(_credit.ValuewithoutPercent);
+
+            DatePickerDayOpen.SelectedDate = _credit.DayOpen;
+            DatePickerDayClose.SelectedDate = _credit.DayClose;
+
+            TextBoxPercent.Text = Convert.ToString(_credit.Percent);
+
+
+            ComboBoxCards.ItemsSource = _storageInstance.Vaults;
+            ComboBoxCards.SelectedItem = _credit.BindedCard;
+
+            ComboBoxPerson.ItemsSource = _storageInstance.Persons;
+            ComboBoxPerson.SelectedItem = _credit.Person;
             UpdateCreditsView();
             Storage.Save();
 
@@ -110,23 +133,109 @@ namespace MoneyHustler.Tabs
 
         }
 
-        private void SelectItem(object sender, MouseButtonEventArgs e)
+
+        private void ButtonBackClick(object sender, RoutedEventArgs e)
         {
 
-            var selecetdCredit = (Credit)listViewForCredits.SelectedItem;
-            TextBoxName.Text = selecetdCredit.Name;
-            TextBoxValueWithoutPercent.Text = Convert.ToString(selecetdCredit.ValuewithoutPercent);
-            DatePickerDayOpen.SelectedDate = selecetdCredit.DayOpen;
-            DatePickerDayClose.SelectedDate = selecetdCredit.DayClose;
-            TextBoxPercent.Text = Convert.ToString(selecetdCredit.Percent);
-            ComboBoxCards.SelectedItem = selecetdCredit.BindedCard;
-            ComboBoxPerson.SelectedItem = selecetdCredit.Person;
-            ButtonAddEdit.Content = "Изменить";
+            ChangeVisibilityOfGridAddEditVault(false);
+            UpdateCreditsView();
+            
+        }
 
-            TextBoxName.IsEnabled = false;
-            TextBoxValueWithoutPercent.IsEnabled = false;
+        private void ButtonSaveClick(object sender, RoutedEventArgs e)
+        {
+
+            decimal enteredValue = 0;
+            decimal enteredPercent = 0;
+            if (TextBoxValueWithoutPercent.IsEnabled == true)
+            {
+                if (TextBoxValueWithoutPercent.Text == String.Empty)
+                {
+                    MessageBox.Show("You need to enter the value!");
+                    return;
+                }
 
 
+                if (!decimal.TryParse(TextBoxValueWithoutPercent.Text, out enteredValue))
+                {
+                    MessageBox.Show("You entered some invalid string to amount field!");
+                    return;
+                }
+
+                if (enteredValue < 0)
+                {
+                    MessageBox.Show("Amount can't be less than zero.");
+                    return;
+                }
+
+                if (ComboBoxCards.SelectedItem == null)
+                {
+                    MessageBox.Show("You need to choose card!");
+                    return;
+                }
+
+                if (DatePickerDayClose.SelectedDate < DatePickerDayOpen.SelectedDate)
+                {
+                    MessageBox.Show("The closing date cannot be earlier than the opening date!");
+                    return;
+                }
+
+
+                if (!decimal.TryParse(TextBoxPercent.Text, out enteredPercent))
+                {
+                    MessageBox.Show("You entered some invalid string to percent field!");
+                    return;
+                }
+
+                if (enteredPercent < 0)
+                {
+                    MessageBox.Show("Percent can't be less than zero.");
+                    return;
+                }
+            }
+
+
+
+            var enteredVault = (Card)ComboBoxCards.SelectedItem;
+
+            if (_credit == null)
+            {
+
+
+                _credit = new Credit(TextBoxName.Text, Convert.ToDouble(TextBoxPercent.Text), null, enteredValue, (Person)ComboBoxPerson.SelectedItem, (Card)ComboBoxCards.SelectedItem, (
+                    DateTime)DatePickerDayClose.SelectedDate, (DateTime)DatePickerDayOpen.SelectedDate);
+                _storageInstance.Credits.Add(_credit);
+
+            }
+            else
+            {
+
+                _credit.Name = TextBoxName.Text;
+                _credit.BindedCard = (Card)ComboBoxCards.SelectedItem;
+                _credit.Person = (Person)ComboBoxPerson.SelectedItem;
+            }
+
+            Storage.Save();
+            
+            ChangeVisibilityOfGridAddEditVault(false);
+            UpdateCreditsView();
+        }
+
+
+
+        private void ChangeVisibilityOfGridAddEditVault(bool visible)
+        {
+            if (visible)
+            {
+                ColumnLabels.Width = new GridLength(20, GridUnitType.Star);
+                ColumnTextBox.Width = new GridLength(20, GridUnitType.Star);
+                
+            }
+            else
+            {
+                ColumnLabels.Width = new GridLength(0, GridUnitType.Star);
+                ColumnTextBox.Width = new GridLength(0, GridUnitType.Star);
+            }
         }
     }
 }
