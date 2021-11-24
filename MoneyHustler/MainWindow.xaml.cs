beeb9b1ea1e46b9b9529882ab47c58babe0e58d0
Arctic Beaver <ArcticBeaver@yandex.ru>
2021-11-24 18:37:51 +0300

    Deleted overrided method and changed main tabs

--- MoneyHustler/MainWindow.xaml.cs
+++ MoneyHustler/MainWindow.xaml.cs
@@ -29,39 +29,6 @@ namespace MoneyHustler
             InitializeComponent();
 
         }
-
-        protected override void OnRenderSizeChanged(System.Windows.SizeChangedInfo sizeInfo)
-        {
-            double mainTabItemWidth;
-            double financeTabControlWidth;
-            double categoriesTabControlWidth;
-
-            int deltaForMainTab = 3;
-            int deltaForFinanceTab = 13;
-
-            foreach (TabItem item in MainTabControl.Items)
-            {
-                mainTabItemWidth = (this.ActualWidth / MainTabControl.Items.Count) - deltaForMainTab;
-                if (mainTabItemWidth < 0) mainTabItemWidth = 0;
-
-                item.Width = mainTabItemWidth;
-            }
-
-            foreach (TabItem item in FinanceTabControl.Items)
-            {
-                financeTabControlWidth = (this.ActualWidth / FinanceTabControl.Items.Count) - deltaForFinanceTab;
-                if (financeTabControlWidth < 0) financeTabControlWidth = 0;
-
-                item.Width = financeTabControlWidth;
-            }
-
-            foreach (TabItem item in TabItemCategoriesName.TabControlCategories.Items)
-            {
-                categoriesTabControlWidth = (this.ActualWidth / FinanceTabControl.Items.Count) - deltaForFinanceTab;
-                if (categoriesTabControlWidth < 0) categoriesTabControlWidth = 0;
-
-                item.Width = categoriesTabControlWidth;
-            }
-        }   
+ 
     }
 }
