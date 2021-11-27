377f34eef83d25de9e36e15dc4051c595ec06a2c
Arctic Beaver <ArcticBeaver@yandex.ru>
2021-11-27 21:24:30 +0300

    Added diagram and main design

--- MoneyHustler/Tabs/TabItemStartTab.xaml.cs
+++ MoneyHustler/Tabs/TabItemStartTab.xaml.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using LiveCharts;
+using LiveCharts.Defaults;
+using LiveCharts.Wpf;
+using MoneyHustler.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +25,47 @@ namespace MoneyHustler.Tabs
     /// </summary>
     public partial class TabItemStartTab : TabItem
     {
+        public SeriesCollection SeriesCollection { get; set; }
+        private Storage _storageInstance = Storage.GetInstance();
+        private ObservableCollection<ExpenseType> _listOfExpenseTypesView;
+        private ObservableCollection<Expense> _listOfExpensesView;
+
+        System.Windows.Controls.DataVisualization.ResourceDictionaryCollection pieSeriesPalette = new System.Windows.Controls.DataVisualization.ResourceDictionaryCollection();
         public TabItemStartTab()
         {
             InitializeComponent();
+            CreateDiagram();
+            DataContext = this;
+        }
+
+        public void CreateDiagram()
+        {
+            _listOfExpenseTypesView = new ObservableCollection<ExpenseType>(_storageInstance.ExpenseTypes);
+            _listOfExpensesView = new ObservableCollection<Expense>(Storage.GetAllExpenses());
+            DateTime pickedPeriod = DateTime.Today;
+
+            if (SeriesCollection == null)
+            {
+                SeriesCollection = new SeriesCollection();
+            }
+
+            foreach (var item in _listOfExpenseTypesView)
+            {
+                var sumExpensesDependingOnType = from i in _listOfExpensesView where i.Type == item && i.Date.Month >= pickedPeriod.Month select i;
+                var value = Math.Round(sumExpensesDependingOnType.Sum(n => n.Amount));
+                if (value == 0)
+                {
+                    continue;
+                }
+                SeriesCollection.Add(new PieSeries { Title = item.Name, Values = new ChartValues<ObservableValue> { new ObservableValue((double)value) }, DataLabels = true }); ;
+            }
+
+            Chart.Update();
+        }
+
+        private void ButtonExitClick(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
         }
     }
 }
