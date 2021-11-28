fb799fe4a2aaa7fb832370257032700e63ccd08f
Nikita <klophah@gmail.com>
2021-11-28 20:45:56 +0300

    added init test for credit

--- MoneyHustlerTests/Models.Tests/CreditTests.cs
+++ MoneyHustlerTests/Models.Tests/CreditTests.cs
@@ -1,9 +1,30 @@
-﻿using NUnit.Framework;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MoneyHustler.Models;
+using NUnit.Framework;
+using System;
 
-namespace MoneyHustlerTests.Models.Tests
+namespace MoneyHustlerTests
 {
-    class CreditTests
+    [TestClass]
+    public class CrediyTests
     {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        public Credit GetTestCredit(int key)
+        {
+            Credit credit = key switch
+            {
+                1 => new Credit("Микрокредитыч", 10, null, 10000, new Person() { Name = "Антон" }, new Card("Кошель", 1000000, 5), new DateTime(2021, 11, 21), new DateTime(2023, 11, 21)),
+                _ => new Credit()
+            };
+            return credit;
+        }
+
+
+        [Test]
+
     }
 }
