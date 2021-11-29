28f57deeeaf056cc42446b97a15bdf07a2d72412
Arctic Beaver <ArcticBeaver@yandex.ru>
2021-11-29 17:10:21 +0300

    OnlyTouUpDepositTests are ready + deleted unn. code

--- MoneyHustler/Models/OnlyTopDeposit.cs
+++ MoneyHustler/Models/OnlyTopDeposit.cs
@@ -21,45 +21,45 @@ namespace MoneyHustler.Models
             DayOfCloseDeposit = dayOfCloseDeposit;
         }
 
-        public new void DecreaseBalance(Expense expense)
-        {
-            if (DateTime.Today > DayOfCloseDeposit)
-            {
-                if (_balance < expense.Amount)
-                {
-                    throw new ArgumentException("You can't decrease your balance with amount more than current balance.");
-                }
-                _balance -= expense.Amount;
-                expense.Vault = this;
-                _expenses.Add(expense);
-            }
-            else
-            {
-                if (MoneyBox < expense.Amount)
-                {
-                    throw new ArgumentException("You can't decrease your balance with amount more than current balance.");
-                }
-                MoneyBox -= expense.Amount;
-                _balance -= expense.Amount;
-                expense.Vault = this;
-                _expenses.Add(expense);
-            }
-            //Если дата закрытия не настала, то мы можем сниять только накопленные деньги,
-            //а после даты закрытия мы можем сниять весь баланс
-        }
+        //public new void DecreaseBalance(Expense expense)
+        //{
+        //    if (DateTime.Today > DayOfCloseDeposit)
+        //    {
+        //        if (_balance < expense.Amount)
+        //        {
+        //            throw new ArgumentException("You can't decrease your balance with amount more than current balance.");
+        //        }
+        //        _balance -= expense.Amount;
+        //        expense.Vault = this;
+        //        _expenses.Add(expense);
+        //    }
+        //    else
+        //    {
+        //        if (MoneyBox < expense.Amount)
+        //        {
+        //            throw new ArgumentException("You can't decrease your balance with amount more than current balance.");
+        //        }
+        //        MoneyBox -= expense.Amount;
+        //        _balance -= expense.Amount;
+        //        expense.Vault = this;
+        //        _expenses.Add(expense);
+        //    }
+        //    //Если дата закрытия не настала, то мы можем сниять только накопленные деньги,
+        //    //а после даты закрытия мы можем сниять весь баланс
+        //}
 
-        public new void EarnIncome()
-        {
-            if (DateTime.Today > DayOfCloseDeposit)
-            {
-                throw new ArgumentException("Deposit is closed"); //Знатоки англиского исправите
-            }
-            if (DateTime.Today.Day == PaymentDay)
-            {
-                MoneyBox = _balance * (Percent / 100)/12;
-                _balance += MoneyBox;
-            }
-        }
+        //public new void EarnIncome()
+        //{
+        //    if (DateTime.Today > DayOfCloseDeposit)
+        //    {
+        //        throw new ArgumentException("Deposit is closed"); //Знатоки англиского исправите
+        //    }
+        //    if (DateTime.Today.Day == PaymentDay)
+        //    {
+        //        MoneyBox = _balance * (Percent / 100)/12;
+        //        _balance += MoneyBox;
+        //    }
+        //}
 
     }
 }
